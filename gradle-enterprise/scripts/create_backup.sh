5ad2e44d235e7e94b0ec16164761047e8ed71cee
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-04-10 07:42:07 +0530

    Delete gradle-enterprise directory

--- gradle-enterprise/scripts/create_backup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-K8S_CMD=kubectl
-K8S_NS_OPT=--namespace=gradle
-DB_POD="$($K8S_CMD $K8S_NS_OPT get pods --selector=app=gradle-enterprise --selector=component=database -o jsonpath='{.items[*].metadata.name}')"
-
-$K8S_CMD $K8S_NS_OPT exec $DB_POD -c database -- /opt/gradle/scripts/system_backup.sh
